b06112e353a03da099b90366e8c1fa6be091ad6e
Daniel <owenjodaniel19@gmail.com>
2023-08-06 18:18:20 +0300

    worked on the features component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Features from './components/Features';
+import NewItems from './components/NewItems';
 
 const App = () => {
   return (
     <div className="w-full max-w-[1440px] mx-auto bg-white">
-      <Hero />
+      <Hero/>
+      <Features/>
+      <NewItems/>
     </div>
   );
 };
