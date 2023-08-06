f15bef3502ff8ebec290a1c54a7a3f914a2371cd
Daniel <owenjodaniel19@gmail.com>
2023-08-06 23:10:15 +0300

    displayed products

--- src/components/ProductSlider.js
+++ src/components/ProductSlider.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import '../slider.css';
 
+import {Navigation, Pagination} from 'swiper'
+import {products} from '../data';
+
+import {HiPlus} from 'react-icons/hi';
 const ProductSlider = () => {
-  return <div>ProductSlider</div>;
+  const { pages } = products
+  return ( 
+    <Swiper modules={[Pagination, Navigation]} pagination={{clickable: true}} navigation={true} className='productSlider min-h-[1300px]'>
+      {pages.map((page, index) => {
+        return (
+           <SwiperSlide key={index}>
+            <div className='grid grid-cols-2 gap-x-5 md:grid-cols-3 lg:grid-cols-4 lg:gap-[30px]'>
+              {page.productList.map((product, index) => {
+                const {image, name, price, oldPrice} = product;
+                return (
+                  <div>
+                    <div>
+                      <img src={image.type} alt=''/>
+                      <div>
+                        <HiPlus className='text-xl text-primary'/>
+                      </div>
+                    </div>
+                    <div>{name}</div>
+                    <div>
+                      <div>$ {price}</div>
+                      <div>$ {oldPrice}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+           </SwiperSlide>
+        )
+      })}
+    </Swiper>
+  );
 };
 
 export default ProductSlider;
