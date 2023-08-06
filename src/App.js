aa4bcf3ea28480203e99ebbe4c35061da2735fe6
Daniel <owenjodaniel19@gmail.com>
2023-08-06 19:24:18 +0300

    displayed the featuresecondary element

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Hero from "./components/Hero";
 import Features from './components/Features';
 import NewItems from './components/NewItems';
-
+import FeaturesSecond from "./components/FeaturesSecond";
 const App = () => {
   return (
     <div className="w-full max-w-[1440px] mx-auto bg-white">
       <Hero/>
       <Features/>
       <NewItems/>
+      <FeaturesSecond/>
     </div>
   );
 };
